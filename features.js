516556c0fc0c11b379b0b5759af3b74331cd4315
Constantinos Stathis <c.stathis@kaizengaming.com>
2022-09-12 15:24:30 +0300

    features list

---
+++ features.js
@@ -0,0 +1,33 @@
+export default {
+  ARES_EXPEDITION: {
+    name: "Ares Expedition",
+    expansionName: "",
+    features: [
+      {
+        id: "tm-complete-player-assistant",
+        name: "Complete player assistant",
+        enabled: false,
+      },
+      { id: "tm-resource-counter", name: "Resources counter", enabled: false },
+      { id: "phases-selector", name: "Phases selector", enabled: false },
+      { id: "oxygen-counter", name: "Oxygen counter", enabled: false },
+      { id: "temp-counter", name: "Temp counter", enabled: false },
+      { id: "oceans-counter", name: "Oceans counter", enabled: false },
+    ],
+  },
+  TERRAFORMING_MARS: {
+    name: "Terraforming Mars",
+    expansionName: "",
+    features: [
+      {
+        id: "complete-player-assistant",
+        name: "Complete player assistant",
+        enabled: false,
+      },
+      { id: "resource-counter", name: "Resources counter", enabled: false },
+      { id: "oxygen-counter", name: "Oxygen counter", enabled: false },
+      { id: "temp-counter", name: "Temp counter", enabled: false },
+      { id: "oceans-counter", name: "Oceans counter", enabled: false },
+    ],
+  },
+};
